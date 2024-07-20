0e043caf309e3a22e86b0a6e2143b685d19ddca9
Nayli <nayliazizanridref@gmail.com>
2024-07-21 00:51:54 +0800

    adjust home.jsx v2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,19 @@ function Home() {
 
             </Grid>
             <Grid className="header-container" item lg={11}>
-                
+                <Grid container diection="row" style={{height: "100%"}}>
+                    <Grid className="left-panel-container" item lg="2.5">
+                        <MyFavouritesPanel 
+                            style={{overflowY: "scroll"}}
+                            handleSetKeyword={handleSetKeyword}
+                            myFavourites={myFavourites}
+                            clearmyFavourites={clearmyFavourites}
+                        ></MyFavouritesPanel>
+                    </Grid>
+                    <Grid className="result-container" item lg="9.5">
+                        <DisplayResults keyWord={keyWord} updateMyFavourites={updateMyFavourites}></DisplayResults>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     );
