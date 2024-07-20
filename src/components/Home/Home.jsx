78a7fa86bed675dd42c2c3629eda101503d8b76d
Nayli <nayliazizanridref@gmail.com>
2024-07-20 23:58:19 +0800

    adjust grid ui

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,14 @@
 function Home() {
+    return (
+        <Grid container className="main-container" direction={"column"}>
+            <Grid className="header-container" item lg={1} style={{maxHeight: "10vh"}}>
 
+            </Grid>
+            <Grid className="header-container" item lg={11}>
+                
+            </Grid>
+        </Grid>
+    );
 }
 
 export default Home;
