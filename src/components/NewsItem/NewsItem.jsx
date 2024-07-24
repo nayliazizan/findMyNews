fd78247bf7c5f425770834d4458790b1a10ded06
Nayli <nayliazizanridref@gmail.com>
2024-07-24 22:10:53 +0800

    adjust ui v5

--- src/components/NewsItem/NewsItem.jsx
+++ src/components/NewsItem/NewsItem.jsx
@@ -1,5 +1,8 @@
-function NewsItem() {
-
+function NewsItem({news}) {
+    const {source, publishedAt, url, urlToImage, title} = news;
+    return (
+        <CustomCard author={source.name} date={publishedAt} url={url} urlToImage={urlToImage} content={title}/>
+    );
 }
 
 export default NewsItem;
--- src/components/display-results/DisplayResults.jsx
+++ src/components/display-results/DisplayResults.jsx
@@ -1,14 +1,50 @@
-import { Typography, Grid } from "@mui/material";
+import { Typography, Grid, Button, LinearProgress } from "@mui/material";
+import NewsItem from "../NewsItem/NewsItem";
 
-function DisplayResults() {
+function DisplayResults({handleLoadMore, searchIsLoading}) {
+    const {keyword, news} = useHomeContext();
+    const title = keyword;
     return (
         <Grid>
-            <Grid container height="100%">
-                <Grid item cs={12}>
-                    <Typography variant="h3">Please search for a topic.</Typography>
+            {news.length === 0 && !searchIsLoading ? (
+                <Grid container height="100%">
+                    <Grid item cs={12}>
+                        {keyword === "" ? (
+                            <Typography variant="h3">Please search for a topic.</Typography>
+                        ) : (
+                            <Typography variant="h3">No news bsed on your keyword. Please try again.</Typography>
+                        )}
+                    </Grid>
                 </Grid>
-            </Grid>
+            ) : (
+                <Grid container spacing={2} direction={"column"} justifyContent={"space-between"} alignItems={"center"}>
+                    <Grid item alignSelf={"start"}>
+                        <Typography variant="h6">Search results for {title}: </Typography>
+                    </Grid>
+
+                    <Grid item>
+                        <Grid container spacing={{xs:1, lg:2}} columns={{xs:4, sm: 8, md: 12, lg: 12}} alignItems="stretch">
+                            {news.map((newsItem, index) => (
+                                <Grid item xs={2} sm={4} md={4} lg={3} key={`${index}-${newsItem.title}`}>
+                                    <NewsItem news={newsItem}/>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Grid>
+
+                    {news.length !== 0 && (
+                        <Grid item marginBottom={1}>
+                            <Button variant="contained" onClick={handleLoadMore}>Load More</Button>
+                        </Grid>
+                    )}
+
+                    <Box sx={{width: "100%", visibility: searchIsLoading ? "visible" : "hidden"}}>
+                        <LinearProgress/>
+                    </Box>
+                </Grid>
+            )}
         </Grid>
+        
     );
 }
 
